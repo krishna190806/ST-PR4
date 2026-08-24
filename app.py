7da8f63477273cdd1a6fa46b786c5761bb48e8fe
Chikhliya Krishna Ramnikbhai <152592270+krishna190806@users.noreply.github.com>
2026-08-24 22:23:18 +0530

    Add arithmetic functions and main demo
    Implement basic arithmetic functions and demo output

---
+++ app.py
@@ -0,0 +1,13 @@
+def add(a, b):
+   return a + b
+
+
+def subtract(a, b):
+   return a - b
+
+def multiply(a, b):
+   return a * b
+
+if __name__ == "__main__":
+   print("Simple CI/CD Demo App")
+   print("2 + 3 =", add(2, 3))
